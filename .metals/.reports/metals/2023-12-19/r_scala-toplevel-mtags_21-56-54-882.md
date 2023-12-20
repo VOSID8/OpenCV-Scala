13537bcac55757efef28b91747e9022069e6cdf8
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2023-12-20 22:06:10 +0530

    Intro

---
+++ .metals/.reports/metals/2023-12-19/r_scala-toplevel-mtags_21-56-54-882.md
@@ -0,0 +1,44 @@
+error id: file:///C:/Projects/OpenCV-Scala/opencv-scala/src/main/scala/Main.scala:[1233..1233) in Input.VirtualFile("file:///C:/Projects/OpenCV-Scala/opencv-scala/src/main/scala/Main.scala", "import javax.swing._
+import org.bytedeco.javacv._
+import org.bytedeco.opencv.global.opencv_core._
+import org.bytedeco.opencv.global.opencv_imgcodecs._
+import org.bytedeco.opencv.global.opencv_imgproc._
+import org.bytedeco.opencv.opencv_core._
+
+
+object 
+
+// object MyFirstOpenCVApp extends App {
+
+//   // Read an image.
+//   val src = imread("data/download.jpg")
+//   display(src, "Input")
+
+//   // Apply Laplacian filter
+//   val dest = new Mat()
+//   Laplacian(src, dest, src.depth(), 1, 3, 0, BORDER_DEFAULT)
+//   display(dest, "Laplacian")
+
+//   //---------------------------------------------------------------------------
+
+//   /** Display `image` with given `caption`. */
+//   def display(image: Mat, caption: String): Unit = {
+//     // Create image window named "My Image."
+//     val canvas = new CanvasFrame(caption, 1)
+
+//     // Request closing of the application when the image window is closed.
+//     canvas.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+
+//     // Convert from OpenCV Mat to Java Buffered image for display
+//     val converter = new OpenCVFrameConverter.ToMat()
+//     // Show image on window
+//     canvas.showImage(converter.convert(image))
+//   }
+// }")
+file:///C:/Projects/OpenCV-Scala/opencv-scala/src/main/scala/Main.scala
+file:///C:/Projects/OpenCV-Scala/opencv-scala/src/main/scala/Main.scala:37: error: expected identifier; obtained eof
+// }
+    ^
+#### Short summary: 
+
+expected identifier; obtained eof
---
+++ src/main/scala/GUIApp.scala.scala
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2011-2019 Jarek Sacha. All Rights Reserved.
+ *
+ * Author's e-mail: jpsacha at gmail.com
+ */
+
+import opencv_cookbook.OpenCVUtils.toBufferedImage
+import org.bytedeco.opencv.global.opencv_core._
+import org.bytedeco.opencv.global.opencv_imgcodecs._
+import org.bytedeco.opencv.global.opencv_imgproc._
+import org.bytedeco.opencv.opencv_core._
+
+import java.awt.Cursor._
+import java.io.File
+import javax.swing.ImageIcon
+import scala.swing.Dialog.Message.Error
+import scala.swing.FileChooser.Result.Approve
+import scala.swing._
+
+object Ex2MyFirstGUIApp extends SimpleSwingApplication {
+
+  private lazy val fileChooser = new FileChooser(new File("."))
+
+  def top: Frame = new MainFrame {
+    title = "My First GUI Scala App"
+
+    // Variable for holding loaded image
+    var image: Option[Mat] = None
+
+
+    //
+    // Define actions
+    //
+
+    // Action performed when "Open Image" button is pressed
+    private val openImageAction = Action("Open Image") {
+      cursor = getPredefinedCursor(WAIT_CURSOR)
+      try {
+        // Load image and update display. If new image was not loaded do nothing.
+        openImage() match {
+          case Some(mat) =>
+            val bi = toBufferedImage(mat)
+            image = Some(mat)
+            imageView.icon = new ImageIcon(bi)
+            processAction.enabled = true
+          case None =>
+        }
+      } finally {
+        cursor = getPredefinedCursor(DEFAULT_CURSOR)
+      }
+    }
+
+    // Action performed when "Process" button is pressed
+    private lazy val processAction = Action("Process") {
+      cursor = getPredefinedCursor(WAIT_CURSOR)
+      try {
+        // Process and update image display if image is loaded
+        image match {
+          case Some(mat) =>
+            processImage(mat)
+            val bi = toBufferedImage(mat)
+            imageView.icon = new ImageIcon(bi)
+          case None =>
+            Dialog.showMessage(null, "Image not opened", title, Error)
+        }
+      } finally {
+        cursor = getPredefinedCursor(DEFAULT_CURSOR)
+      }
+    }
+    processAction.enabled = false
+
+    //
+    // Create UI
+    //
+
+    // Component for displaying the image
+    lazy val imageView = new Label
+
+    // Create button panel
+    private val buttonsPanel = new GridPanel(rows0 = 0, cols0 = 1) {
+      contents += new Button(openImageAction)
+      contents += new Button(processAction)
+      vGap = 5
+    }
+
+    // Layout frame contents
+    contents = new BorderPanel() {
+      // Action buttons on the left
+      add(new FlowPanel(buttonsPanel), BorderPanel.Position.West)
+      // Image display in the center
+      private val imageScrollPane = new ScrollPane(imageView) {
+        preferredSize = new Dimension(640, 480)
+      }
+      add(imageScrollPane, BorderPanel.Position.Center)
+    }
+
+    // Mark for display in the center of the screen
+    centerOnScreen()
+  }
+
+
+  /** Ask user for location and open new image. */
+  private def openImage(): Option[Mat] = {
+    // Ask user for the location of the image file
+    if (fileChooser.showOpenDialog(null) != Approve) {
+      return None
+    }
+
+    // Load the image
+    val path = fileChooser.selectedFile.getAbsolutePath
+    val newImage = imread(path)
+    if (!newImage.empty()) {
+      Some(newImage)
+    } else {
+      Dialog.showMessage(null, "Cannot open image file: " + path, top.title, Error)
+      None
+    }
+  }
+
+
+  /** Process image in place. */
+  private def processImage(src: Mat): Unit = {
+    // Flip upside down
+    flip(src, src, 0)
+    // Swap red and blue channels
+    cvtColor(src, src, COLOR_BGR2RGB)
+  }
+}
